a6fad5223ae55e098b3e427df7643588febb7e92
JeffTunner <abhinavkaushik05@gmail.com>
2026-04-01 20:16:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import NavBar from "./components/NavBar.jsx";
+
+function App() {
+    return (
+        <main>
+            <NavBar />
+        </main>
+    );
+}
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <header>
+            <nav>
+                <img src={"/logo.svg"} alt={"apple-logo"} />
+
+                <ul>
+                    {[]}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+export default NavBar
